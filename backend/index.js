c0447e42c29528246a7f601fe21594b31ee2891d
Adityasharma6220 <adityasharma6220@gmail.com>
2024-10-11 17:08:47 +0530

    cors added

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://jobhunt-ayqj.onrender.com',
+    origin:'https://thejobportal0.netlify.app',
     credentials:true
 }
 
